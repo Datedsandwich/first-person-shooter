663a975d781a2192173ef4667db3a7a82ab45d9a
Luke Robinson <robinsonlm@live.co.uk>
2017-04-17 10:03:40 +0100

    RaycastShoot script, can now fire the gun but nothing occurs as a result of this

---
+++ Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+	public int gunDamage = 1;
+	public float fireRate = 0.25f;
+	public float weaponRange = 50;
+	public float hitForce = 100;
+	public Transform gunEnd;
+
+	private Camera fpsCam;
+	private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+	private AudioSource gunAudio;
+	private LineRenderer laserLine;
+	private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+		laserLine = GetComponent<LineRenderer>();
+		gunAudio = GetComponent<AudioSource>();
+		fpsCam = GetComponentInParent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) {
+			Fire ();
+		} else {
+			DrawDebugRay ();
+		}
+	}
+
+	private void Fire () {
+		nextFire = Time.time + fireRate;
+		StartCoroutine (ShotEffect ());
+		FireRaycast ();
+	}
+
+	private void FireRaycast () {
+		RaycastHit hit;
+		laserLine.SetPosition (0, gunEnd.position);
+		if (Physics.Raycast (getRayOrigin(), fpsCam.transform.forward, out hit, weaponRange)) {
+			laserLine.SetPosition (1, hit.point);
+		}
+		else {
+			laserLine.SetPosition (1, fpsCam.transform.forward * weaponRange);
+		}
+	}
+
+	private void DrawDebugRay() {
+		Debug.DrawRay (getRayOrigin (), fpsCam.transform.forward * weaponRange, Color.green);
+	}
+
+	private IEnumerator ShotEffect() {
+		gunAudio.Play ();
+		laserLine.enabled = true;
+
+		yield return shotDuration;
+
+		laserLine.enabled = false;
+	}
+
+	private Vector3 getRayOrigin() {
+		return fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+	}
+}
